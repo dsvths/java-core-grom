28b8ca442e2d1ebacb7ad0514b2a2dde3752b7a1
dsvths <dsvths@gmail.com>
2017-06-22 11:48:14 +0300

    error correction in classes EO & FO homework lesson10

--- src/lesson10/homeWork/ElectronicsOrder.java
+++ src/lesson10/homeWork/ElectronicsOrder.java
@@ -26,9 +26,11 @@ public class ElectronicsOrder extends Order {
 
     @Override
     public void validateOrder() {
-        if (getBasePrice() >= 100 && getCustomerOwned().getGender() =="Женский");
-        if (getCustomerOwned().getCity() == "Киев" || getCustomerOwned().getCity() =="Одесса" || getCustomerOwned().getCity() =="Днепр" || getCustomerOwned().getCity() =="Харьков");
-        if (getShipToCity() == "Киев" || getShipToCity() == "Днепр" || getShipToCity() == "Одесса" || getShipToCity() == "Харьков");
+        if (getBasePrice() >= 100 && getCustomerOwned().getGender() == "Женский") ;
+        if (getCustomerOwned().getCity() == "Киев" || getCustomerOwned().getCity() == "Одесса" || getCustomerOwned().getCity() == "Днепр" || getCustomerOwned().getCity() == "Харьков")
+            ;
+        if (getShipToCity() == "Киев" || getShipToCity() == "Днепр" || getShipToCity() == "Одесса" || getShipToCity() == "Харьков")
+            ;
 
         setDateConfirmed(new Date());
     }
@@ -36,10 +38,16 @@ public class ElectronicsOrder extends Order {
     @Override
     public void calculatePrice() {
         double price = 0;
-        if (getBasePrice() >= 5000) {
-            price = getBasePrice() + getBasePrice()*0.02;
+        double comission = 0;
+        if (getShipToCity() == "Киев" || getShipToCity() == "Одесса") {
+            comission = getBasePrice() + getBasePrice() * 0.1;
+
+        } else comission = getBasePrice() + getBasePrice() * 0.15;
+
+        if (getBasePrice() >= 1000) {
+            price = (getBasePrice() + comission) - (getBasePrice() + comission) * 0.05;
         }
-        price = getBasePrice() + getBasePrice()*0.05;
+        price = getBasePrice() + comission;
 
         setTotalPrice(price);
 
