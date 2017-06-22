aaddc6f4ded0f70166daa02ba2752b3780eae423
dsvths <dsvths@gmail.com>
2017-06-22 11:14:18 +0300

    error correction in classes Customer & Demo homework lesson10

--- src/lesson10/homeWork/Customer.java
+++ src/lesson10/homeWork/Customer.java
@@ -19,4 +19,10 @@ public class Customer {
     public String getGender() {
         return gender;
     }
+
+    public Customer(String name, String city, String gender) {
+        this.name = name;
+        this.city = city;
+        this.gender = gender;
+    }
 }
--- src/lesson10/homeWork/Demo.java
+++ src/lesson10/homeWork/Demo.java
@@ -7,25 +7,25 @@ import java.util.Date;
  */
 public class Demo {
     public static void main(String[] args) {
-        ElectronicsOrder tv = new ElectronicsOrder("Sony", new Date(), "Одесса", 2500, new Customer(), 12);
+        ElectronicsOrder tv = new ElectronicsOrder("Sony", new Date(), "Одесса", 2500, new Customer("Nataly", "Одесса", "Женский"), 12);
         tv.validateOrder();
         tv.calculatePrice();
         tv.confirmShipping();
 
 
-        ElectronicsOrder phone = new ElectronicsOrder("IPhone 7", new Date(), "Киев", 21300, new Customer(), 6);
+        ElectronicsOrder phone = new ElectronicsOrder("IPhone 7", new Date(), "Киев", 21300, new Customer("Ольга", "Киев", "Женский"), 6);
         phone.validateOrder();
         phone.calculatePrice();
         phone.confirmShipping();
 
 
-        FurnitureOrder furnitureOrder = new FurnitureOrder("Cupboard", new Date(), "Львов", 7900, new Customer(), "7");
+        FurnitureOrder furnitureOrder = new FurnitureOrder("Cupboard", new Date(), "Львов", 7900, new Customer("Василий", "Донецк", "Мужской"), "7");
         furnitureOrder.validateOrder();
         furnitureOrder.calculatePrice();
         furnitureOrder.confirmShipping();
 
 
-        FurnitureOrder furnitureOrder1 = new FurnitureOrder("Table", new Date(), "Киев", 5200, new Customer(), "9");
+        FurnitureOrder furnitureOrder1 = new FurnitureOrder("Table", new Date(), "Киев", 5200, new Customer("Роман", "Кропивницкий", "Мужской"), "9");
         furnitureOrder1.validateOrder();
         furnitureOrder1.calculatePrice();
         furnitureOrder1.confirmShipping();
