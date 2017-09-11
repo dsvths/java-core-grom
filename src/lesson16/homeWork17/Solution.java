f7a956d5c5fc61b2a23238fae000383acf31b025
dsvths <dsvths@gmail.com>
2017-09-11 16:56:53 +0300

    homework1 to lesson17

---
+++ src/lesson16/homeWork17/Solution.java
@@ -0,0 +1,49 @@
+package lesson16.homeWork17;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by Savenko on 11.09.2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+        String test = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 354 sed do eiusmod 18 tempor incididunt ut && labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+
+        //System.out.println(check(test));
+
+        System.out.println(countWords(test));
+    }
+
+    public static int countWords(String input) {
+
+        String[] words = input.split(" ");
+
+        int[] res = new int[words.length];
+
+        //check(words);
+
+
+
+        int count = 0;
+
+
+        for (int i = 0; i < words.length; i++)
+            if (check(words[i]))
+                count++;
+        return count;
+    }
+
+    private static boolean check (String str){
+        char[] chars = str.toCharArray();
+
+        for (char c : chars){
+            if (!Character.isLetter(c))
+                return false;
+        }
+        return true;
+    }
+
+}
+
